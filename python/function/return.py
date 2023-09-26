3592b1fffb865bad31a4eca62bc8ef549a188349
Sayma Akther <108178686+sayma-akther@users.noreply.github.com>
2023-09-26 09:28:09 +0600

    Add files via upload

---
+++ python/function/return.py
@@ -0,0 +1,13 @@
+def sum_2_number(first, second):
+    return first + second
+
+
+result = sum_2_number(10, 15)
+print(f"Summation result is : {result}")
+
+
+def substraction_2_no(third, forth):
+    return third - forth
+
+result2 = substraction_2_no(30,6)
+print(f"Result is : {result2}")
