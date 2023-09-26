2c24d3e801d94ce156ffc1603d8e5c2042ddb9aa
Sayma Akther <108178686+sayma-akther@users.noreply.github.com>
2023-09-26 09:17:10 +0600

    Add files via upload

---
+++ python/function/first_pro.py
@@ -0,0 +1,13 @@
+def my_name_is():
+    print("Sayma Akther")
+
+
+def i_have_enrolled_course(course_name):
+    print(f"I have enrolled in a course named {course_name}")   
+
+def i_am_learning(backend,fontend):
+    return f"Learning {fontend} {backend}" 
+my_name_is()
+i_have_enrolled_course("Androdid App")
+result = i_am_learning("Java", "kotlin")
+print(result)
---
+++ python/function/oparation.py
@@ -0,0 +1,30 @@
+#Added 2 number using function
+
+def sum_2_number(first, second):
+    print(f"Sum of {first} + {second} = {first + second}")
+
+
+#Subtract 2 number using function
+def sub_2_num(a, b):
+    print(f"Subtraction of {a} - {b} = {a - b}")
+
+
+#Multiplecation 4 number using function
+def multi_4_num(p, q, r, s):
+    print(f"Multiplication of {p} * {q} * {r} * {s} = {p* q* r* s}")  
+
+
+#Division 2 number using function
+def divied_2_num(x, y):
+    print(f"Division of {x} / {y} = {x / y}")  
+
+
+
+
+sum_2_number(10, 15)
+sub_2_num(100, 60)
+multi_4_num(2, 9, 7, 5)
+divied_2_num(100,2)
+
+
+
